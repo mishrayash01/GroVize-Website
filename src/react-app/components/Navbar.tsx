af8d30c59b5fcc705fefd3f56d3c434ed7cb08b5
mishrayash01 <yashmishra1102005@gmail.com>
2026-02-04 15:52:31 +0530

    fix: adjust navbar spacing to prevent overlapping elements

--- src/react-app/components/Navbar.tsx
+++ src/react-app/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
-          <Link to="/" className="flex items-center gap-2">
+          <Link to="/" className="flex items-center gap-2 flex-shrink-0">
             <img 
               src={logo} 
               alt="GroVize Logo" 
@@ -46,7 +46,7 @@ export default function Navbar() {
           </Link>
 
           {/* Desktop Navigation */}
-          <div className="hidden lg:flex items-center space-x-8">
+          <div className="hidden lg:flex items-center space-x-4">
             {navLinks.map((link) => (
               <Link
                 key={link.path}
